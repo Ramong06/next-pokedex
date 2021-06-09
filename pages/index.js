887ce40cded826322e4ef729dd275f2fdf61f9a6
ramon-poetic <ramon@poetic.io>
2021-06-09 17:24:49 -0500

    fetched pokemon data from api

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/Head'
 import Layout from "../components/Layout";
 
 
-export default function Home() {
+export default function Home({pokemon}) {
+  console.log(pokemon);
   return (
     <Layout title="Next Pokedex">
       <h1 className="text-4xl mb-8 text-center ">Next Pokedex </h1>
@@ -12,7 +13,19 @@ export default function Home() {
 
 
 export async function getStaticProps(context) {
-  return {
-    props: {}
+  try {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=300');
+      const { results } = await res.json();
+      const pokemon = results.map((result, index) => {
+          const paddedId = ('00' + (index + 1)).slice(-3);
+
+          const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+          return { ...result, image };
+      });
+      return {
+          props: { pokemon },
+      };
+  } catch (err) {
+      console.error(err);
   }
 }
